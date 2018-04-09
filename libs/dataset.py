e3aa6bbc0b8701fcf07d626ad8cd59904ae9816a
AlexGAzagnes <a_syoez@yahoo.fr>
2018-04-09 12:00:43 +0200

    change v2 from libs files

--- libs/dataset.py
+++ libs/dataset.py
@@ -2,227 +2,113 @@
 #-*- coding: utf8 -*-
 
 
-###############################
-#	dataset.py
-###############################
-
-
-#
-#
-#
-#
-#
-
 
 ###############################
-# 	IMPORT
+###############################
+#   dataset
+###############################
 ###############################
 
 
-import bokeh.plotting as bkp
-import bokeh.layouts as bkl
-import os
+
+# hhuiefuiaefzueofea
+# fazilfauefaozffazf
+# jfizafziafuafziaofa
+# fzailflizeoafa
+# fhaiufiozfeafoa"""
+
+
+
+#########################
+#   IMPORTS 
+#########################
+
+
 import pandas as pd
 from stockstats import StockDataFrame
 
 
-##############################
-# 	CLASS
-###############################
+
+#########################
+#   CLASS 
+#########################
 
 
-class _DataSet(pd.DataFrame):
-    """pandas Dataset : collection of data from an ohlc file"""
+class DataSet(): 
 
-    def __init__(self, data_dict, filename, name, value, curency,	period, interval):
-
-        pd.DataFrame.__init__(self, data_dict)
-        self.filename = filename
-        self.name = filename.replace(".csv", "")
-        self.value = value
-        self.curency = curency
-        self.period = period
-        self.interval = interval
-        self.desc = "{} in {} for {} in {}".format(value, curency, period, interval)
-        self.main_indicators = dict()
-        self.other_indicators = dict()
-        self.orders = list()
-        self.result = (self["close"].iloc[-1] - self["close"].iloc[0])/self["close"].iloc[0] \
-            if self["close"].iloc[-1] > self["close"].iloc[0] \
-            else -(self["close"].iloc[0] - self["close"].iloc[-1])/self.close[0]
-
-    def add_indicator(self, name, val, main=True):
-        if main:
-            self.main_indicators[name] = [val]
-        else:
-            self.other_indicators[name] = [val]
-
-    def add_orders(self, buy, sell):
-        self["buy"] = pd.Series(buy)
-        self["sell"] = pd.Series(sell)
-        self.orders = ["buy", "sell"]
-
-    def add_ma(self, period, mode="s", base="close"):
-        u""" add one or various standard/exponentiel moving average to self (df.DataFrame)
+    def __init__(self, filename, name=None, value=None, 
+                 curency=None, period=None, interval=None) : 
+        u""" aDataset 
 
         positional args :
-                period :    period to calulate ma
-                                        type int for one ma or list of int for various ma
+
+            filename :  filename of your CVS file
+                        type str, exemple "google.csv",
+                        only CSV files suported for now
 
         optional args :
-                mode :      standard, or exponential moving average (or both)
-                                        type str "s" for std, "e" for exp, "b" for both
-
-                base :      value based caluclation
-                                        type str "open", "high", "low", "close", "middle"
-                                        or "o", "h", "l", "c", "m"
-
-        return :        none, add expected attribute(s) to self + update self.inidcators
-        """
-
-        stock = StockDataFrame.retype(self)
-
-        # manage period argument
-        if type(period) != list:
-            period = [period]
-        for p in period:
-            if not (type(p) == int and 0 <= p <= 10000):
-                raise ValueError("int expected")
-
-        # manage mode argument
-        if mode not in ["s", "e", "b", "std", "exp", "both"]:
-            raise ValueError("s, e, b expected")
-        mode = [mode]
-        if mode[0] == ("b" or "both"):
-            mode = ["e", "s"]
-
-        # manage base argument
-        base_dict = {"o": "open", "h": "high", "l": "low", "c": "close", "m": "middle"}
-        if base not in (base_dict.keys() + base_dict.values()):
-            raise ValueError("o, h, l, c, m expected")
-        if base in base_dict.keys():
-            base = base_dict[base]
-
-        indicator_list = list()
-        # add ma(s) regading given arguments
-        for m in mode:
-            for p in period:
-                att = "{}_{}_{}".format(base, p, m+"ma")
-                self[att] = stock.get(att).round(2)
-                indicator_list.append(att)
-        self.main_indicators["ma"] = indicator_list
-
-    def add_bollingers(self):
-
-        stock = StockDataFrame.retype(self)
-        self["boll"] = stock['boll'].round(2)
-        self.main_indicators["bollingers"] = ["boll_ub", "boll_lb"]
-
-    def add_cross(self, indicator_1, indicator_2, option=""):
-
-        stock = StockDataFrame.retype(self)
-        att = indicator_1 + "_x" + option + "_" + indicator_2
-
-        self[att] = stock.get(att)
-        self.main_indicators["cross"] = [att]
-
-    def add_macd(self, spec=""):
-        u""" add MACD value, signal or Histogram to self (df.DataFrame)
-
-        optional args :
-                spec :      MACD's type
-                                        type str or list of str: "v" for value, "s" for signal line, "h" for histogram
-                                        default or "a"or "all": all
-                                        spec = "" or spec = "v"
-
-        return :        none, add expected attribute(s) to self + update self.indicators
-        """
-
-        stock = StockDataFrame.retype(self)
-        indicator_list = list()
-
-        self["macds"] = stock["macds"].round(2)
-        self["macdh"] = stock["macdh"].round(2)
-
-        indicator_list.append("macd")
-        if spec:
-            indicator_list.append("macd_" + spec)
-
-        self.other_indicators["macd"] = indicator_list
-
-    def add_rsi(self, period):
-        u""" add RSI value,for one or various period
-
-        positional
-         args :
-                period :    MACD's type
-                                        type int or list of int
-
-        return :        none, add expected attribute(s) to self + update self.other_indicators
-        """
-
-        stock = StockDataFrame.retype(self)
-        indicator_list = list()
-
-        if type(period) != list:
-            period = [period]
-        for p in period:
-            att = "{}_{}".format("rsi", p)
-            self[att] = stock.get(att).round(2)
-            indicator_list.append(att)
-        self.other_indicators["rsi"] = indicator_list
-
-    def add_volatiliy():  #  MSTD: moving standard deviation or MVAR: moving variance
-        pass
-
-    def add_stochastic():  # RSV or KDJ
-        pass
-
-    def add_vvr():  # VR: Volatility Volume Ratio
-        pass
-
-
-###############################
-# 	FUNCTIONS
-###############################
-
-
-def DataSet(filename, name="", value="", curency="", period="", interval=""):
-    u""" add standard or exponentiel moving average to self.stock
-
-    positional args :
-
-            filename :	filename of your CVS file
-                                    type str, exemple "google.csv",
-                                    only CSV files suported for now
-
-    optional args :
-            name : 		arbitrary name of your dataset
-                                    type str, default is filname without ".csv"
+            name :      arbitrary name of your dataset
+                        type str, default is filname without ".csv"
 
             value :     stock's name, or value/asset you work on
-                                    type str, exemple "BTC", "Gold", "Google"
+                        type str, exemple "BTC", "Gold", "Google"
 
-            currency : 	currency
-                                    type str, exemple "$", "eur", "BTC"
+            currency :  currency
+                        type str, exemple "$", "eur", "BTC"
 
-            period : 	period you work on :
-                                    type str, exemple "01/01/2016 -01/01/18"
-                                    if "auto" : automatic calculation
+            period :    period you work on :
+                        type str, exemple "01/01/2016 -01/01/18"
+                        if "auto" : automatic calculation
 
-            interval :	standard internationnal interval reference
-                                    D for day H for hour, T for minute
+            interval :  standard internationnal interval reference
+                        D for day H for hour, T for minute
 
 
     return :        an DataSet object, based on DataFrame but ehanced of various features"""
 
-    # manage args
-    if not os.path.isfile(filename):
-        raise ValueError("file not found in {}".format(path))
-    if not ".csv" in filename:
-        raise ValueError("only CSV file suported for now")
 
-    # call the _DataSet consturctor
-    return _DataSet(	pd.read_csv(filename).to_dict(),
-                     filename, name, value, curency,
-                     period, interval)
+        if not name : 
+            self.name = filename.replace(".csv")
+        else : 
+            self.name=name
+
+        if not value : 
+            pass # self.value = try_to_get_value(filename)
+        else : 
+            self.value=value
+        
+        if not curency : 
+            pass # self.curency = try_to_get_curency(filename)
+        else : 
+            self.curency = curency
+
+        self.data = StockDataFrame.retype(pd.read_csv(filename))
+
+        if not period : 
+            pass # self.period = try_to_get_period(filename)
+        else :
+            self.period = period
+
+        if not interval : 
+            pass # self.interval = try_to_get_interval(filename)
+        else : 
+            self.interval = interval
+
+
+
+#########################
+#   MAIN
+#########################
+
+
+if __name__ == '__main__':
+
+    ds = DataSet("/home/alex/Bureau/beta_backtrader/datas/accord.csv",
+                name="accord_eur_2017_1D", value="accord",
+                curency="eur", period="2017", interval="1D")
+
+    print(ds.__dict__.keys())
+    print(ds.data.head())
+    print(ds.data.shape)
+    print(ds.data.columns)
+    print(len(ds.data))
--- libs/strategy.py
+++ libs/strategy.py
@@ -3,87 +3,226 @@
 
 
 
-
 ###############################
-#	strategy.py
+###############################
+#	strategy
+###############################
 ###############################
 
-stg = Strategy(	name="my_first_strategy", 
-				family= "indicator_cross", 
-				dataset=df,
-				conditions={"buy" : "close_10_sma_xu_close_30_sma", 
-							"sell": None, 
-							"stop_loss" : 0.01, 
-							"stop_profit" : 0.04, 
-							"waiter": 0})
+
+
+# hhuiefuiaefzueofea
+# fazilfauefaozffazf
+# jfizafziafuafziaofa
+# fzailflizeoafa
+# fhaiufiozfeafoa"""
+
+
+
+#########################
+#	IMPORTS 
+#########################
+
+
+from __future__ import division
+
+
+
+#########################
+#	CLASS
+#########################
+
 
 class Strategy : 
 
-		def __init__(self, name, family, dataset, **conditions)
+	def __init__(self, name="my_first_strategy", family= "indicator_cross",
+					order_type="market", price="close",
+					dataset="dataset",
+					conditions={"buy": "close_10_sma_xu_close_30_sma", 
+								"sell": None, 
+								"stop_loss_profit_base": "real",
+								"stop_loss": 0.01, "trailing_stop_loss": None,
+								"stop_profit":0.04,"trailing_stop_profit":None,
+								"universal_waiter": 0, "loss_waiter": None, 
+								"profit_waiter": None}) : 
+
 
 		# strat ID
 		self.name 	= name
 		self.family = family
-
-		# manage conditions
-		try : self.buy = conditions["buy"]
-		except : raise ValueError("error conditions strategy, not good Buy so Goodbye")
-		
-		if (not ("sell" or "stop_profit")) in conditions.keys() : 
-			raise : ValueError("error conditions strategy, not good Sell")
-		else : 
-			try : self.sell = conditions["sell"]
-			except: self.sell = None
-			try  : self.stop_profit = conditions["stop_profit"]
-
-		try : self.stop_loss = conditions["stop_loss"]
-		except: self.stop_loss = -1.1
-		
-		try : self.waiter = conditions["waiter"]
-		except : self.waiter = 0
-
+		self.order_type = order_type
+		self.price = price
+		self.df = dataset.data
 		# add an indicator for curent trade --> stop loss and stop profit
+		
 		self.last_buy = 0
 		self.curent_price = 0
 		self.curent_trade = 0
 		
+		self.buy_condition = conditions["buy"]
+		# if (not conditions["sell"]) or (not isinstance(conditions["sell"], str)) : 
+		# 	conditions["sell"] = str(conditions["sell"])
+		self.sell_condition = conditions["sell"]
 
-		# try to add technicals indicators
-		if self.buy : 
-			try : dataset[self.buy]
-			except : raise ValueError("error conditions strategy, not good Buy so Goodbye")
+		self.stop_loss_profit_base = conditions["stop_loss_profit_base"]
 
-		if self.sell : 
-			try : dataset[self.sell]
-			except : raise : ValueError("error conditions strategy, not good Sell")
-			
+		self.stop_loss = conditions["stop_loss"]
+		self.stop_profit = conditions["stop_profit"]
+		self.trailing_stop_loss = conditions["trailing_stop_loss"]
+		self.trailing_stop_profit = conditions["trailing_stop_profit"]
 
+		self.universal_waiter = conditions["universal_waiter"]
+		self.loss_waiter = conditions["loss_waiter" ]
+		self.profit_waiter = conditions["profit_waiter"]
 
-	def eval_curent_trade() : 
-		self.curent_price = dataset.loc[i, "close"]
-		if self.curent_price > self.last_buy : 
-			self.curent_trade = (self.curent_price - self.last_buy) / self.last_buy
-		elif  self.curent_price > self.last_buy : 
-			self.curent_trade = - (self.last_buy - self.curent_price ) / self.last_buy
-		else : 
-			self.curent_trade = 0
+		self.add_needed_indicators()
+		#		print(self.df.head())
 
 
+	def add_needed_indicators(self) : 
 
-	def strat_say_buy(i) : 
-		if dataset.loc[i, self.buy] : 
-			self.last_buy = dataset.loc[i, "close"]
-			return True
-		else : return False 
-
-
-	def start_say_sell(i) : 
-		eval_curent_trade()
-		if self.curent_trade > self.stop_profit : return True
-		elif (-self.curent_trade) > self.stop_loss : return True
-		elif dataset.loc[i, self.sell] : return True
-		else : return False 
+		for indicator in [self.buy_condition, self.sell_condition] : 
+			if indicator : 
+				try : 
+					self.df.get(indicator)
+				except : 
+					self.df["None"] = False
+					self.sell_condition = "None"
+			else : 
+				self.df["None"] = False
+				self.sell_condition = "None"
 
 
 
 
+	def says_buy(self, i) : 
+		if self.df.loc[i, self.buy_condition] : 
+			self.curent_trade = 0.0
+			return True, str(self.buy_condition)
+		self.curent_trade = 0.0
+		return False, None 
+
+
+	def says_sell(self, i, k) : 
+		sell, reason = self.automatic_sell(i,k)
+		if sell : 
+			return True, reason 
+		if self.df.loc[i, self.sell_condition] : 
+			return True, str(self.sell_condition)
+		return False, None
+
+
+	def automatic_sell(self, i, k):
+		self.curent_trade = k[1] if self.stop_loss_profit_base=="real" else k[0]
+		if (self.curent_trade > self.stop_profit) : 
+			return True, "stop profit"
+		if (-self.curent_trade) > self.stop_loss : 
+			return True, "stop loss"
+		return False, None
+	
+
+
+#########################
+#	MAIN 
+#########################
+
+
+if __name__ == '__main__':
+
+
+	#########################
+	#	IMPORTS
+	#########################
+
+	from user import User
+	from broker import Broker
+	from dataset2 import DataSet
+	from trading_room2 import TradingRoom
+
+
+
+	#########################
+	#	FUNCTIONS 
+	#########################
+
+
+	def print_label(label) : 
+		print("\n\n############################################")
+		print("\t{}".format(label))
+		print("############################################\n")
+
+
+
+	#########################
+	#	MAIN
+	#########################
+
+
+	# initiate objects
+	###################
+
+
+	ds = DataSet("/home/alex/Bureau/beta_backtrader/datas/accord.csv", 
+				name="accord_eur_2017_1D", value="accord", 
+				curency="eur", period="2017", interval="1D") 
+
+
+	brk = Broker( name="test_broker", 
+				  maker_fees=0.002, 
+				  taker_fees=0.004,
+				  slipage=0.005)
+
+
+	stg = Strategy(	name="my_first_strategy", family= "indicator_cross",
+					order_type="market", price="close",
+					dataset=ds,
+					conditions={"buy": "close_10_sma_xu_close_50_sma", 
+								"sell": None, 
+								"stop_loss_profit_base": "real",
+								"stop_loss": 0.5, "trailing_stop_loss": None,
+								"stop_profit":0.1,"trailing_stop_profit":None,
+								"universal_waiter": None, "loss_waiter": None, 
+								"profit_waiter": None})
+
+
+
+	usr = User(bank=100, ruined_rate=0.99)
+
+
+	# launch trading session
+	##########################
+
+
+	trading_session = TradingRoom(dataset=ds, 
+								  strategy=stg,
+								  broker=brk, 
+								  user=usr)
+
+	# _ = raw_input("Go for control prints?\n")
+
+
+	# control prints
+	###################
+
+
+	# dataset info 
+	print_label("Dataset info")
+	print(ds.data.iloc[:3, :])
+	print(ds.data.iloc[-3:, :])
+
+	
+	# trading session info 
+	print_label("trading session info")
+	print(trading_session)
+
+
+	# trading session results
+	print_label("trading session results")
+	trading_session.print_results()
+
+
+
+	# trading session orders
+	print_label("trading session orders")
+	mask = ds.data.loc[:, "orders"] != 0
+	print(ds.data.loc[mask, ["close", "orders", "abs_result", "real_result", "order_signal"]])
