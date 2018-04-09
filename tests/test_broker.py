36bcd7039600e63e2759dccadda54fede04a00b0
Alexandre GAZAGNES <a_syoez@yahoo.fr>
2018-04-09 13:32:51 +0200

    first test for broker with pytest

---
+++ tests/test_broker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python 
+#-*- coding: utf8 -*-
+
+
+
+###############################
+###############################
+#	  test_broker.py
+###############################
+###############################
+
+
+
+#  import 
+
+import pytest
+
+
+
+# main
+
+def test_broker() : 
+    broker = Broker() : 
+    assert  broker
