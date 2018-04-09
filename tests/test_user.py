98d03503b5958abd659c7ff452010cd3c5acd167
Alexandre GAZAGNES <a_syoez@yahoo.fr>
2018-04-09 13:34:06 +0200

    first test for user with pytest

---
+++ tests/test_user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python 
+#-*- coding: utf8 -*-
+
+
+
+###############################
+###############################
+#	  test_user.py
+###############################
+###############################
+
+
+
+#  import 
+
+import pytest
+
+
+
+# main
+
+def test_user() : 
+    user = User() 
+    assert user
