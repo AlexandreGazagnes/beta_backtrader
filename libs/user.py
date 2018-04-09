2ccbd71607d632dad926e632531073932b572f7f
Alexandre GAZAGNES <a_syoez@yahoo.fr>
2018-04-09 14:48:48 +0200

    major changes in doctstring, MAX and MIN values

--- libs/user.py
+++ libs/user.py
@@ -11,8 +11,8 @@
 
 
 
-# this is the object reprentation of a user agent see doctstring bellow for full information
-
+# user is the python object representation of a real user agent
+# see full information in bellow docstring
 
 
 
@@ -22,15 +22,59 @@
 
 
 class User : 
-	u""" user representation 
-	TO DO : add positional and optional args, return statement
-		describe more detail infromation about user integration in main
-		add vision, agressivity and mood feature of user 
-	"""
+    u"""User is a object that simulate and store important features of an user
+	this feature is not drasticly usefull for now but will contain advanced
+	and very dramatic features for future versions
+	TO DO for futures version : 
+	- 	manage "agressivity" of an user regarding money management strategy 
+	- 	manage some king of "mood" or "market vision". is the user optimistic, 
+		realistic pessimist, want kind of impact this feature will affect 
+		its strategy choice, is it possible to train our agent to have a good
+		"market vision" ? (...), for the same strategy what impact could or should
+		have this "market vision" regarding final result? (...)
 
-	def __init__(self, bank, ruined_rate) : 
-		self.bank = bank
-		self.ruined_rate = ruined_rate
+    """
+
+	MIN = {"bank":100, "ruined_rate" : 0.1}
+	MAX = {"bank":10000, "ruined_rate" : 0.99}
+
+	def __init__(self, bank=MIN["bank"], ruined_rate=max["ruined_rate"]) : 
+        
+        u"""Initialization.
+
+            positional arguments :
+
+            bank        :       the arbitray sum your user have on his bank account
+                                type int in [100, 10_000]
+
+            ruined_rate  :      regarding a wrong strategy, the level your 
+            					user will decide to stop the simulation and will
+            					consider himself "ruined" 
+            					type ploat in [0.0, 1.0]
+
+
+        """
+
+        # validation of bank arguments regarding default and authorised values
+        try : 
+        	bank = int(bank)
+        	assert self.MAX["bank"] >= bank >= self.MIN["bank"]
+        	self.bank = bank
+        except:
+            raise ValueError(
+                "User.bank : given {}, expected int, min {}, max {}"
+                .format(bank, self.MIN["bank"], self.MAX["bank"]))
+
+        # validation of ruined rate arguments regarding default and authorised values
+        try : 
+        	ruined_rate = int(ruined_rate)
+        	assert self.MAX["ruined_rate"] >= ruined_rate >= self.MIN["ruined_rate"]
+        	self.ruined_rate = ruined_rate
+
+        except:
+            raise ValueError(
+                "User.ruined_rate : given {}, expected float, min {}, max {}"
+                .format(ruined_rate, self.MIN["ruined_rate"], self.MAX["ruined_rate"]))
 
 
 
@@ -39,5 +83,8 @@ class User :
 #########################
 
 
+# this section should be founded in test/test_user.py (with pytest usage)
+
 if __name__ == '__main__':
-	print("not implemented :) ")
+	user = User()
+	print(user.__dict__)
